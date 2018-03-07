5b7f2d76947899c2d4ea7a4d77fbff4c80d69f56
Victor Debone <victor@debone.com.br>
2018-03-07 01:10:18 +0100

    #11 Created event model

--- timebudget/models.py
+++ timebudget/models.py
@@ -5,3 +5,8 @@ class Timeslot(models.Model):
     startTime = models.IntegerField()
     dayOfTheWeek = models.IntegerField()
     duration = models.IntegerField()
+
+class Event(models.Model):
+    timeslot = models.ForeignKey(Timeslot, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    frequency = models.IntegerField()
